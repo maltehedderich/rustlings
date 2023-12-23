ad757606a974ae5394370bfb16947970bcb17f82
Malte Hedderich <github@hedderich.pro>
2023-12-23 14:50:23 +0100

    complete quiz1

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,14 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(amount: u32) -> u32 {
+    if (amount > 40) {
+        amount
+    } else {
+        amount * 2
+    }
+}
 
 // Don't modify this function!
 #[test]
